e34947c344ec272f97e6d060a90fe6eae4524985
vanessafso <vanessa.jay.vf@gmail.com>
2023-08-21 18:36:34 -0300

    Ajustado sintaxe vpc_security_group_ids

--- ec2.tf
+++ ec2.tf
@@ -65,7 +65,7 @@ resource "aws_instance" "metabase" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.jenkins.id]
+  vpc_security_group_ids = [aws_security_group.metabase.id]
   subnet_id              = aws_subnet.this["pvt_b"].id
   availability_zone      = "${var.aws_region}b"
 
